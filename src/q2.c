463d98ab8ba1cf2650afd22684d96d0c5b9cbe35
aaryaaa07 <aaryathapa078@gmail.com>
2026-03-13 08:37:57 +0545

    assignment 3

--- src/q2.c
+++ src/q2.c
@@ -1,1 +1,28 @@
 // Implement a C program to reverse the elements of an array.
+
+#include <stdio.h>
+
+int main() {
+    int n, min, max, temp;
+    printf("enter the size of array:");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("enrter elements of array:");
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+    printf("the elemetns of array are:");
+    for(int i=0; i<n; i++) printf("%d", arr[i]);
+    printf("\n");
+
+    for(int i=0; i<n/2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n-1-i];
+        arr[n-1-i]= temp ;
+
+    }
+    printf("the elements of array after reversing are:");
+    for(int i=0; i<n; i++) printf("%d", arr[i]);
+    printf("\n");
+    return 0;
+}
--- src/q3.c
+++ src/q3.c
@@ -1,1 +1,36 @@
 // Write a C program to sort an array of integers in ascending order using a sorting algorithm of your choice (e.g., bubble sort, selection sort, insertion sort).
+#include <stdio.h>
+
+int main() {
+    int n, min, max, temp;
+    printf("enter the size of array:");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("enter elements of array:");
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+
+    printf("the elemetns of array are: ");
+    for(int i=0; i<n; i++) 
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    for(int i=0; i < n - 1; i++) 
+    {
+        for (int j=0; j< n - 1- i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            
+            }
+        }
+    }
+     printf("the elemetns of array after sort are: ");
+    for(int i=0; i<n; i++) printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}    
